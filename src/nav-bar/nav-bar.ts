ef32ff528d44f87a686d3fb8f9b2a972163417f2
Dimitris Loukas <d.loukas@Athens.BrokerSystems.gr>
2016-04-19 10:22:41 +0300

    Added nav-bar

---
+++ src/nav-bar/nav-bar.ts
@@ -0,0 +1,8 @@
+/** View Model for global site nav bar */
+import {customElement, bindable} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
+
+@customElement('nav-bar')
+export class NavBar{
+    @bindable router: Router;
+}
