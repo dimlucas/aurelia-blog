1d168e9dc1a968486ace23cb24100c05a5a950b5
Dimitris Loukas <d.loukas@Athens.BrokerSystems.gr>
2016-04-19 09:45:18 +0300

    Initial Commit

---
+++ dist/app.js
@@ -0,0 +1,16 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    var App = (function () {
+        function App() {
+        }
+        App.prototype.configureRouter = function (config, router) {
+            config.title = 'Aurelia';
+            config.map([]);
+            this.router = router;
+        };
+        return App;
+    }());
+    exports.App = App;
+});
+//# sourceMappingURL=app.js.map
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwic291cmNlUm9vdCI6Ii9zcmMiLCJzb3VyY2VzIjpbImFwcC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztJQUVBO1FBQUE7UUFXQSxDQUFDO1FBUkMsNkJBQWUsR0FBZixVQUFnQixNQUEyQixFQUFFLE1BQWM7WUFDekQsTUFBTSxDQUFDLEtBQUssR0FBRyxTQUFTLENBQUM7WUFDekIsTUFBTSxDQUFDLEdBQUcsQ0FBQyxFQUVWLENBQUMsQ0FBQztZQUVILElBQUksQ0FBQyxNQUFNLEdBQUcsTUFBTSxDQUFDO1FBQ3ZCLENBQUM7UUFDSCxVQUFDO0lBQUQsQ0FBQyxBQVhELElBV0M7SUFYWSxXQUFHLE1BV2YsQ0FBQSJ9
