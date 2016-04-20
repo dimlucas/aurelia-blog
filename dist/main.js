d3fd2c4967e87a6c087cf0941610bf2130be7f9b
Dimitris Loukas <d.loukas@Athens.BrokerSystems.gr>
2016-04-20 14:32:57 +0300

    Resolved versioning context

--- dist/main.js
+++ dist/main.js
@@ -3,10 +3,11 @@ define(["require", "exports", 'bootstrap'], function (require, exports) {
     function configure(aurelia) {
         aurelia.use
             .standardConfiguration()
-            .developmentLogging();
+            .developmentLogging()
+            .plugin('aurelia-dialog');
         aurelia.start().then(function () { return aurelia.setRoot(); });
     }
     exports.configure = configure;
 });
 //# sourceMappingURL=main.js.map
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZVJvb3QiOiIvc3JjIiwic291cmNlcyI6WyJtYWluLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0lBR0EsbUJBQTBCLE9BQWdCO1FBQ3hDLE9BQU8sQ0FBQyxHQUFHO2FBQ1IscUJBQXFCLEVBQUU7YUFDdkIsa0JBQWtCLEVBQUUsQ0FBQztRQVF4QixPQUFPLENBQUMsS0FBSyxFQUFFLENBQUMsSUFBSSxDQUFDLGNBQU0sT0FBQSxPQUFPLENBQUMsT0FBTyxFQUFFLEVBQWpCLENBQWlCLENBQUMsQ0FBQztJQUNoRCxDQUFDO0lBWmUsaUJBQVMsWUFZeEIsQ0FBQSJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZVJvb3QiOiIvc3JjIiwic291cmNlcyI6WyJtYWluLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0lBR0EsbUJBQTBCLE9BQWdCO1FBQ3hDLE9BQU8sQ0FBQyxHQUFHO2FBQ1IscUJBQXFCLEVBQUU7YUFDdkIsa0JBQWtCLEVBQUU7YUFDcEIsTUFBTSxDQUFDLGdCQUFnQixDQUFDLENBQUM7UUFRNUIsT0FBTyxDQUFDLEtBQUssRUFBRSxDQUFDLElBQUksQ0FBQyxjQUFNLE9BQUEsT0FBTyxDQUFDLE9BQU8sRUFBRSxFQUFqQixDQUFpQixDQUFDLENBQUM7SUFDaEQsQ0FBQztJQWJlLGlCQUFTLFlBYXhCLENBQUEifQ==
