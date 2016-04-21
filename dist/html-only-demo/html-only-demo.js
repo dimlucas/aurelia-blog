1ebf04dd16c348e232967f0da1fefceb4ed63a0e
Dimitris Loukas <d.loukas@Athens.BrokerSystems.gr>
2016-04-21 12:05:16 +0300

    HTML-only custom element

---
+++ dist/html-only-demo/html-only-demo.js
@@ -0,0 +1,12 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    var HtmlOnlyDemo = (function () {
+        function HtmlOnlyDemo() {
+            this.email = "";
+        }
+        return HtmlOnlyDemo;
+    }());
+    exports.HtmlOnlyDemo = HtmlOnlyDemo;
+});
+//# sourceMappingURL=html-only-demo.js.map
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaHRtbC1vbmx5LWRlbW8vaHRtbC1vbmx5LWRlbW8uanMiLCJzb3VyY2VSb290IjoiL3NyYyIsInNvdXJjZXMiOlsiaHRtbC1vbmx5LWRlbW8vaHRtbC1vbmx5LWRlbW8udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7SUFBQTtRQUFBO1lBRUksVUFBSyxHQUFXLEVBQUUsQ0FBQztRQUV2QixDQUFDO1FBQUQsbUJBQUM7SUFBRCxDQUFDLEFBSkQsSUFJQztJQUpZLG9CQUFZLGVBSXhCLENBQUEifQ==
--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,8 @@ export class App {
         { route: 'login', name: 'login', moduleId: './auth/login', nav: true, title: 'Login' },
         { route: 'view-info', name: 'view-info', moduleId: './user-info/view-info', nav: true, title: 'View Personal Information' },
         { route: 'set-info', name: 'set-info', moduleId: './user-info/set-info', nav: true, title: 'Update Personal Information' },
-        { route: 'dialog-demo', name: 'dialog-demo', moduleId: './dialog-demo/dialog-demo', nav: true, title: 'Dialog Demo' }
+        { route: 'dialog-demo', name: 'dialog-demo', moduleId: './dialog-demo/dialog-demo', nav: true, title: 'Dialog Demo' },
+        { route: 'html-demo', name: 'html-demo', moduleId: './html-only-demo/html-only-demo', nav: true, title: 'HTML Only Elements' }
     ]);
 
     this.router = router;
